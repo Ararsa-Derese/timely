9cd6314cd00829449638bf511dd255784e59c366
Bisrat Kebere <kiyyakebere@gmail.com>
2024-02-21 04:49:54 +0900

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Providers } from './providers'
 import { Toaster } from 'react-hot-toast'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 export function generateViewport() {
   return {
@@ -27,8 +28,10 @@ export default function RootLayout({
         <Toaster />
         <Providers>
           {children}
+        <Analytics />
         </Providers>
       </body>
     </html>
   )
 }
+ 
